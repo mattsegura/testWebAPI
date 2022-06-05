3f78113045abd029feab078f05dc62070411ab51
Matthew <Matthew@DESKTOP-3ECDB05>
2022-06-04 22:34:48 -0500

    added deletion

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -52,5 +52,33 @@ namespace SuperHero.Controllers
             heroes.Add(hero);
             return Ok(heroes); // return to make sure everything is fine 
         }
+
+        [HttpPut]
+
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request) // show values within swagger
+        {
+            var hero = heroes.Find(h => h.Id == request.Id); // searching for the hero Id within method
+            if (hero == null) // checking hero 
+                return BadRequest("Hero not found");
+
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+
+            return Ok(heroes); // return to make sure everything is fine 
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult<List<SuperHero>>> Delete(int id) // show values within swagger
+        {
+            var hero = heroes.Find(h => h.Id == id); // searching for the hero Id within method
+            if (hero == null) // checking hero 
+                return BadRequest("Hero not found");
+
+            heroes.Remove(hero);
+            return Ok(heroes); // return to make sure everything is fine 
+        }
     }
 }
